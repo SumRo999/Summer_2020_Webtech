ce06d6d1010cc974f6787736afca6e6441e36524
Sumaiya Sultana <sumaiya.sultana.369248@gmail.com>
2020-08-04 09:27:10 +0600

    login

--- LAB_MID_XM/logcheck.php
+++ LAB_MID_XM/logcheck.php
@@ -15,25 +15,23 @@
 			$data = fread($file, filesize('user.txt'));
 			$user = explode('|', $data);
 
-
 			while(!feof($data)){
 				$user = fgets($data);
 				$user = explode('|', $data);
-			}
-
-			//print_r($user);
-
-			if(trim($user[0]) == $userId && trim($user[1]) == $password){
+				if(trim($user[0]) == $userId && trim($user[1]) == $password){
 				$_SESSION['status']  = "Ok";
-				header('location: home.php');
-			}else{
-				echo "Invalid username/password";
+				header('location:home.php');
+			    }
+				else
+				{
+				    echo "Invalid userId/password";
+			    }
 			}
+
+
 		}
 
 	}else{
-		header("location: login.html");
+		header("location:login.html");
 	}
-
-
 ?>
