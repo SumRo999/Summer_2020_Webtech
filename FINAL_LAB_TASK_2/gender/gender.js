510d954137c3a203d3e915cb8f39dd0a409e3378
Sumaiya Sultana <sumaiya.sultana.369248@gmail.com>
2020-08-24 21:33:08 +0600

    gender js

---
+++ FINAL_LAB_TASK_2/gender/gender.js
@@ -0,0 +1,23 @@
+function validate()
+{
+	var show = document.getElementById("show");
+	var male = document.getElementById("male").checked;
+	var female = document.getElementById("female").checked;
+	var other = document.getElementById("other").checked;
+	if( male == true || female == true || other == true)
+	{
+		return true;
+	}
+	else
+	{
+		show.innerHTML = "|At least one of them has to be selected";
+		return false;
+	}
+	
+}
+
+function clicks()
+{
+	var show = document.getElementById("show");
+	show.innerHTML = "";
+}
