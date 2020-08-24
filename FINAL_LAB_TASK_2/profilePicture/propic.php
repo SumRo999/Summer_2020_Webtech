d0a729558c29240f0c76ec3c136a0b60beddde06
Sumaiya Sultana <sumaiya.sultana.369248@gmail.com>
2020-08-24 23:15:38 +0600

    profile picture php

---
+++ FINAL_LAB_TASK_2/profilePicture/propic.php
@@ -0,0 +1,12 @@
+<?php
+   if(isset($_POST['submit']))
+   {
+   		$id = $_POST['id'];
+   	    $propic = $_FILES['image']['name'];
+   	    echo "Form submitted<br>";
+   	    echo "User ID is $id<br>";
+   	    echo "Profile picture name $propic";
+   }
+   else
+   	   header('location:name.html')
+?>
