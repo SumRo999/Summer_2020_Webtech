a2eacb501b0e2b71beab13f1a76453cc72d1929a
Sumaiya Sultana <sumaiya.sultana.369248@gmail.com>
2020-09-06 08:32:47 +0600

    index page

---
+++ Final_Lab_Exam/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index  Page</title>
+</head>
+<body>
+	<a href="views/adminlogin.php">Login as Admin</a> |
+	<a href="views/authorlogin.php">Login as Author</a>
+</body>
+</html>
